400602847df381e87a27e3a5d6e86fedf67d79db
Angus Taylor <angustaylor@Anguss-MacBook-Pro.local>
2021-08-18 18:30:47 +0100

    Minor fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
 
   def booking_must_have_ended?
     if booking.end_date < Date.today
-    errors.add(:base, 'Sorry but you cannot leave a review as your booking has not happened yet')
+      errors.add(:base, 'Sorry but you cannot leave a review as your booking has not happened yet')
     end
   end
 end
