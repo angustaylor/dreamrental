ebfa804378d18e8b825eb29ed415375b605826c3
Angus Taylor <angustaylor@Anguss-MacBook-Pro.local>
2021-08-19 11:59:45 +0100

    fix to review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   validate :booking_must_have_ended?
 
   def booking_must_have_ended?
-    if booking.end_date < Date.today
+    if booking.end_date > Date.today
       errors.add(:base, 'Sorry but you cannot leave a review as your booking has not happened yet')
     end
   end
